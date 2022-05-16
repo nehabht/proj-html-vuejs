f7a883fbd6efb62f25dcbeee641ad29a5ff86897
nehabht <ne.bhatti2@icloud.com>
2022-05-16 16:08:20 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+    <MainComponent />
+    <FooterComponent />
+
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '@/components/HeaderComponent.vue'
+import MainComponent from '@/components/MainComponent.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+ 
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/style.scss";
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="info-contacts">
+        <div class="header-left">
+            
+
+        </div>
+        <div class="header-right">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
